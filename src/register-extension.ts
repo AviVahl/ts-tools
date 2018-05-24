7946dcb2514f95aa325b4e56940b9e0097ad1bc8
Avi Vahl <avi.vahl@wix.com>
2018-05-24 19:40:05 +0300

    Install source-map-support when registered this activates source-maps for all transpiled ts files, for proper dev experience

--- src/register-extension.ts
+++ src/register-extension.ts
@@ -1,4 +1,14 @@
+import { install } from 'source-map-support'
 import { TypeScriptNodeExtension } from './ts-node-extension'
+import { sourceMaps } from './global-state'
 
 // register our handler for the two supported extensions
 require.extensions['.ts'] = require.extensions['.tsx'] = TypeScriptNodeExtension
+
+install({
+    environment: 'node',
+    retrieveSourceMap(filePath): any /* Until PR to DefinitelyTyped is merged */ {
+        const fileSourceMap = sourceMaps.get(filePath)
+        return fileSourceMap ? { map: fileSourceMap, url: filePath } : null
+    }
+})
