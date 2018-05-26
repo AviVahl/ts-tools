995824331e9b9392b100051cb452272eed5f5247
Avi Vahl <avi.vahl@wix.com>
2018-05-26 20:30:01 +0300

    Clarify why internal _compile is exposed

--- src/node-internals.d.ts
+++ src/node-internals.d.ts
@@ -1,3 +1,5 @@
 declare interface NodeModule {
+    // internal js compilation method, used by the require extension
+    // to evalute transpiled code
     _compile(code: string, filePath: string): void
 }
