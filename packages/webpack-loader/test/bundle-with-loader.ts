1e033f71cb017a9c11d3cb0520e57b858cbc0cac
Avi Vahl <avi.vahl@wix.com>
2018-10-08 02:37:31 +0300

    Upgrade to @types/react@16.4.16 - also removed memory-fs and replaced with noop output fs

--- packages/webpack-loader/test/bundle-with-loader.ts
+++ packages/webpack-loader/test/bundle-with-loader.ts
@@ -1,5 +1,5 @@
 import webpack from 'webpack'
-import MemoryFS from 'memory-fs'
+import { join } from 'path'
 import { ITypeScriptLoaderOptions, tsService } from '../src'
 
 export interface IBundleWithLoaderOptions {
@@ -48,11 +48,20 @@ export async function bundleWithLoader(
     })
 
     // so test output isn't saved on local hard drive
-    compiler.outputFileSystem = new MemoryFS()
+    compiler.outputFileSystem = noopOutputFileSystem
 
     const stats = await new Promise<webpack.Stats>((res, rej) => {
         compiler.run((e, s) => e ? rej(e) : res(s))
     })
 
     return { stats, statsText: stats.toString() }
+}
+
+const noopOutputFileSystem: webpack.OutputFileSystem = {
+    join,
+    mkdir(_path, callback) { callback(null) },
+    mkdirp(_path, callback) { callback(null) },
+    rmdir(_path, callback) { callback(null) },
+    unlink(_path, callback) { callback(null) },
+    writeFile(_path, _data, callback) { callback(null) }
 }
