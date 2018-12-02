33e47bfbf172222dd988f9299b0203ee870f4e55
Avi Vahl <avi.vahl@wix.com>
2018-12-02 21:41:28 +0200

    @ts-tools/build: initial implementation

---
+++ packages/build/src/build.ts
@@ -0,0 +1,158 @@
+import path from 'path'
+import chalk from 'chalk'
+import { createBaseHost, createLanguageServiceHost } from '@ts-tools/service'
+import ts from 'typescript'
+
+export interface IOutputFile {
+    /**
+     * Expected absolute path of file
+     */
+    filePath: string
+
+    /**
+     * Generated contents of file.
+     */
+    contents: string
+}
+
+export interface IBuildFormat {
+    /**
+     * Name of directory to create for the format.
+     */
+    folderName: string
+
+    /**
+     * Callback that returns the compiler options to be used when transpiling.
+     *
+     * @param tsconfigOptions existing options found in tsconfig
+     */
+    getCompilerOptions(tsconfigOptions: Readonly<ts.CompilerOptions>): ts.CompilerOptions
+}
+
+export interface IBuildOptions {
+    /**
+     * Absolute path to src directory, which should contain a `tsconfig.json` file.
+     */
+    srcDirectoryPath: string
+
+    /**
+     * Absolute path to output directory, where formats folders are created.
+     */
+    outputDirectoryPath: string
+
+    /**
+     * Formats to build.
+     */
+    formats: IBuildFormat[]
+}
+
+export function build(options: IBuildOptions): IOutputFile[] {
+    const { formats, outputDirectoryPath, srcDirectoryPath } = options
+    const tsConfigPath = path.join(srcDirectoryPath, 'tsconfig.json')
+
+    const baseHost = createBaseHost(process.cwd())
+    const { dirname, fileExists, directoryExists, readFile } = baseHost
+
+    if (!directoryExists(srcDirectoryPath)) {
+        throw chalk.red(`Cannot find directory ${srcDirectoryPath}`)
+    } else if (!fileExists(tsConfigPath)) {
+        throw chalk.red(`Cannot find ${tsConfigPath}`)
+    }
+
+    // read and parse config
+    const jsonSourceFile = ts.readJsonConfigFile(tsConfigPath, readFile)
+    const configDirectoryPath = dirname(tsConfigPath)
+
+    const {
+        errors,
+        fileNames,
+        options: tsconfigOptions
+    } = ts.parseJsonSourceFileConfigFileContent(jsonSourceFile, baseHost, configDirectoryPath)
+
+    if (errors.length) {
+        throw ts.formatDiagnosticsWithColorAndContext(errors, baseHost)
+    }
+
+    const documentRegistry = ts.createDocumentRegistry(
+        baseHost.useCaseSensitiveFileNames,
+        baseHost.getCurrentDirectory()
+    )
+    const formatCompilers: Array<{ folderName: string, languageService: ts.LanguageService }> = []
+
+    for (const { folderName, getCompilerOptions } of formats) {
+        const compilerOptions: ts.CompilerOptions = {
+            ...getCompilerOptions(tsconfigOptions),
+            outDir: undefined,
+            outFile: undefined,
+            out: undefined,
+            noEmit: false
+        }
+        const languageServiceHost = createLanguageServiceHost(
+            baseHost,
+            fileNames,
+            compilerOptions
+        )
+        const languageService = ts.createLanguageService(
+            languageServiceHost,
+            documentRegistry
+        )
+        formatCompilers.push({
+            folderName,
+            languageService
+        })
+    }
+
+    const syntacticDiagnostics: ts.Diagnostic[] = []
+    const semanticDiagnostics: ts.Diagnostic[] = []
+    const outputFiles: IOutputFile[] = []
+
+    for (const { folderName, languageService } of formatCompilers) {
+        const formatOutDir = path.join(outputDirectoryPath, folderName)
+        for (const srcFilePath of fileNames) {
+            const nativeSrcFilePath = path.normalize(srcFilePath)
+
+            arrayAssign(syntacticDiagnostics, languageService.getSyntacticDiagnostics(srcFilePath))
+            arrayAssign(semanticDiagnostics, languageService.getSemanticDiagnostics(srcFilePath))
+
+            const { emitSkipped, outputFiles: compilationOutput } = languageService.getEmitOutput(srcFilePath)
+            if (!emitSkipped) {
+                for (const { name: outputFilePath, text } of compilationOutput) {
+                    const relativeToSrc = path.relative(srcDirectoryPath, outputFilePath)
+                    const targetFilePath = path.join(formatOutDir, relativeToSrc)
+                    const targetFileDirectoryPath = path.dirname(targetFilePath)
+                    const relativeRequestToSrc = path.relative(targetFileDirectoryPath, nativeSrcFilePath)
+                    const contents = outputFilePath.endsWith('.map') ? remapSourceMap(text, relativeRequestToSrc) : text
+                    outputFiles.push({
+                        filePath: targetFilePath,
+                        contents
+                    })
+                }
+            }
+        }
+        if (syntacticDiagnostics.length) {
+            throw ts.formatDiagnosticsWithColorAndContext(syntacticDiagnostics, baseHost)
+        } else if (semanticDiagnostics.length) {
+            throw ts.formatDiagnosticsWithColorAndContext(semanticDiagnostics, baseHost)
+        }
+    }
+
+    return outputFiles
+}
+
+function arrayAssign<T>(targetArr: T[], sourceArr: T[]): void {
+    for (const item of sourceArr) {
+        targetArr.push(item)
+    }
+}
+
+function remapSourceMap(originalSourceMap: string, mappedSrcRequest: string): string {
+    try {
+        const sourceMap = JSON.parse(originalSourceMap)
+        if (Array.isArray(sourceMap.sources) && sourceMap.sources.length === 1) {
+            sourceMap.sources[0] = mappedSrcRequest
+        }
+        return JSON.stringify(sourceMap)
+    } catch {
+        return originalSourceMap
+    }
+}
---
+++ packages/build/src/cli.ts
@@ -0,0 +1,71 @@
+// tslint:disable:no-console no-var-requires
+import ts from 'typescript'
+import path from 'path'
+import { writeFileSync, ensureDirectorySync } from 'proper-fs'
+import chalk from 'chalk'
+import program from 'commander'
+import { build, IBuildFormat } from './build'
+
+const { version } = require('../package.json')
+process.on('unhandledRejection', printErrorAndExit)
+
+program
+    .version(version, '-v, --version')
+    .description('Build multi-target TypeScript libraries.')
+    .usage('<src folder>')
+    .option('--out-dir <output folder>', 'output directory', '.')
+    .option('--cjs', 'compile a "cjs" folder containing commonjs module target')
+    .option('--esm', 'compile an "esm" folder containing esnext module target')
+    .parse(process.argv)
+
+const { args, cjs, esm, outDir } = program
+
+if (args.length !== 1) {
+    printErrorAndExit(chalk.red(`A single src folder has to be provided`))
+} else if (!cjs && !esm) {
+    printErrorAndExit(chalk.red(`Must specify --cjs, --esm, or both`))
+}
+
+const [srcDirName] = args
+const srcDirectoryPath = path.resolve(srcDirName)
+const outputDirectoryPath = path.resolve(outDir)
+
+const formats: IBuildFormat[] = []
+if (cjs) {
+    formats.push({
+        folderName: 'cjs',
+        getCompilerOptions(tsconfigOptions) {
+            return {
+                ...tsconfigOptions,
+                module: ts.ModuleKind.CommonJS
+            }
+        }
+    })
+}
+if (esm) {
+    formats.push({
+        folderName: 'esm',
+        getCompilerOptions(tsconfigOptions) {
+            return {
+                ...tsconfigOptions,
+                module: ts.ModuleKind.ESNext
+            }
+        }
+    })
+}
+
+try {
+    const targetFiles = build({ srcDirectoryPath, outputDirectoryPath, formats })
+    console.log(`Done transpiling. Writing ${targetFiles.length} files...`)
+    for (const { filePath, contents: content } of targetFiles) {
+        ensureDirectorySync(path.dirname(filePath))
+        writeFileSync(filePath, content)
+    }
+} catch (e) {
+    printErrorAndExit(e)
+}
+
+function printErrorAndExit(message: unknown) {
+    console.error(message)
+    process.exit(1)
+}
