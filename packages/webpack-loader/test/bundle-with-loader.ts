296bc5ea39d5e5a3b40b85e66bf74ce3c6ec9aae
Avi Vahl <avi.vahl@wix.com>
2020-10-14 18:31:51 +0300

    test: loader tests to use recommended setup while our mocked noop-fs worked, it was an ugly hack. a bit cleaner now (only an incompatible type).

--- packages/webpack-loader/test/bundle-with-loader.ts
+++ packages/webpack-loader/test/bundle-with-loader.ts
@@ -1,7 +1,7 @@
-import { join, dirname } from 'path';
+import { dirname } from 'path';
 import webpack from 'webpack';
 import type { ITypeScriptLoaderOptions } from '@ts-tools/webpack-loader';
-
+import { createFsFromVolume, Volume } from 'memfs';
 export interface IBundleWithLoaderOptions {
   entry: string;
   options?: ITypeScriptLoaderOptions;
@@ -30,7 +30,7 @@ export async function bundleWithLoader({
   });
 
   // so test output isn't saved on local hard drive
-  compiler.outputFileSystem = (noopOutputFileSystem as unknown) as import('webpack').Compiler['outputFileSystem'];
+  compiler.outputFileSystem = createFsFromVolume(new Volume()) as webpack.Compiler['outputFileSystem'];
 
   const stats = await new Promise<webpack.Stats>((res, rej) => {
     compiler.run((e, s) => (e ? rej(e) : res(s)));
@@ -38,25 +38,3 @@ export async function bundleWithLoader({
 
   return { stats, statsText: stats.toString() };
 }
-
-const noopOutputFileSystem = {
-  join,
-  mkdir(_path: string, callback: () => void) {
-    callback();
-  },
-  mkdirp(_path: string, callback: () => void) {
-    callback();
-  },
-  rmdir(_path: string, callback: () => void) {
-    callback();
-  },
-  stat(_path: string, callback: (e?: Error) => void) {
-    callback(new Error(`ENOENT`));
-  },
-  unlink(_path: string, callback: () => void) {
-    callback();
-  },
-  writeFile(_path: string, _data: unknown, callback: () => void) {
-    callback();
-  },
-};
