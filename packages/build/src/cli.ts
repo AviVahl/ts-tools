030d793a7d935b92e4a6160d40f93eb655881734
Avi Vahl <avi.vahl@wix.com>
2021-01-17 14:47:07 +0200

    refactor(build): replace commander with yargs - has nicer typings (not just any) - also fixed a tiny output issue when src folder isn't provided

--- packages/build/src/cli.ts
+++ packages/build/src/cli.ts
@@ -3,77 +3,79 @@ import { writeFileSync, statSync, mkdirSync } from 'fs';
 
 import ts from 'typescript';
 import chalk from 'chalk';
-import program from 'commander';
+import yargs from 'yargs';
 import { build, IBuildFormat } from './build';
 
 // eslint-disable-next-line
-const { version } = require('../package.json');
-process.on('unhandledRejection', printErrorAndExit);
+const { version } = require('../package.json') as { version: string };
+process.on('unhandledRejection', printErrorAndMarkFailed);
 
-program
-  .version(version, '-v, --version')
-  .description('Build multi-target TypeScript libraries.')
-  .usage('<src folder>')
-  .option('--out-dir <output folder>', 'output directory', '.')
-  .option('--config-name <config file name>', 'tsconfig file name', 'tsconfig.json')
-  .option('--cjs', 'compile a "cjs" folder containing commonjs module target')
-  .option('--esm', 'compile an "esm" folder containing esnext module target')
-  .parse(process.argv);
+const { argv } = yargs
+  .usage(`Usage: $0 <src folder> [options]`)
+  .version(version)
+  .option('out-dir', { default: '.', description: 'output directory' })
+  .option('config-name', { default: 'tsconfig.json', description: 'tsconfig file name' })
+  .option('cjs', { type: 'boolean', description: 'compile a "cjs" folder containing commonjs module target' })
+  .option('esm', { type: 'boolean', description: 'compile an "esm" folder containing esnext module target' })
+  .alias('h', 'help')
+  .alias('v', 'version')
+  .help()
+  .strict();
 
-const { args, cjs, esm, outDir, configName } = program;
+const { _, 'config-name': configName, 'out-dir': outDir, cjs, esm } = argv;
 
-if (args.length !== 1) {
-  printErrorAndExit(chalk.red(`A single src folder has to be provided`));
+if (_.length !== 1) {
+  printErrorAndMarkFailed(chalk.red(`A single <src folder> has to be provided.`));
 } else if (!cjs && !esm) {
-  printErrorAndExit(chalk.red(`Must specify --cjs, --esm, or both`));
-}
+  printErrorAndMarkFailed(chalk.red(`Must specify --cjs, --esm, or both`));
+} else {
+  const [srcDirName] = _ as [string | number];
+  const srcDirectoryPath = resolve(`${srcDirName}`);
+  const outputDirectoryPath = resolve(outDir);
 
-const [srcDirName] = args;
-const srcDirectoryPath = resolve(srcDirName!);
-const outputDirectoryPath = resolve(outDir);
-
-const formats: IBuildFormat[] = [];
-if (cjs) {
-  formats.push({
-    folderName: 'cjs',
-    getCompilerOptions(tsconfigOptions) {
-      return {
-        ...tsconfigOptions,
-        module: ts.ModuleKind.CommonJS,
-      };
-    },
-  });
-}
-if (esm) {
-  formats.push({
-    folderName: 'esm',
-    getCompilerOptions(tsconfigOptions) {
-      return {
-        ...tsconfigOptions,
-        module: ts.ModuleKind.ESNext,
-      };
-    },
-  });
-}
-
-try {
-  const targetFiles = build({
-    srcDirectoryPath,
-    outputDirectoryPath,
-    formats,
-    configName: configName as string | undefined,
-  });
-  // eslint-disable-next-line no-console
-  console.log(`Done transpiling. Writing ${targetFiles.length} files...`);
-  for (const { name, text } of targetFiles) {
-    ensureDirectorySync(dirname(name));
-    writeFileSync(name, text);
+  const formats: IBuildFormat[] = [];
+  if (cjs) {
+    formats.push({
+      folderName: 'cjs',
+      getCompilerOptions(tsconfigOptions) {
+        return {
+          ...tsconfigOptions,
+          module: ts.ModuleKind.CommonJS,
+        };
+      },
+    });
   }
-} catch (e) {
-  printErrorAndExit(e);
+  if (esm) {
+    formats.push({
+      folderName: 'esm',
+      getCompilerOptions(tsconfigOptions) {
+        return {
+          ...tsconfigOptions,
+          module: ts.ModuleKind.ESNext,
+        };
+      },
+    });
+  }
+
+  try {
+    const targetFiles = build({
+      srcDirectoryPath,
+      outputDirectoryPath,
+      formats,
+      configName,
+    });
+    // eslint-disable-next-line no-console
+    console.log(`Done transpiling. Writing ${targetFiles.length} files...`);
+    for (const { name: filePath, text } of targetFiles) {
+      ensureDirectorySync(dirname(filePath));
+      writeFileSync(filePath, text);
+    }
+  } catch (e) {
+    printErrorAndMarkFailed(e);
+  }
 }
 
-function printErrorAndExit(message: unknown) {
+function printErrorAndMarkFailed(message: unknown) {
   // eslint-disable-next-line no-console
   console.error(message);
   process.exitCode = 1;
