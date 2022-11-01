322ffddbeb39cd73acfe230cbf87ba459b8233de
Avi Vahl <avi.vahl@wix.com>
2022-11-01 21:06:55 +0200

    infra: drop node 14 and use @types/node@18

--- packages/esm/src/test/esm-loader.test.ts
+++ packages/esm/src/test/esm-loader.test.ts
@@ -6,7 +6,6 @@ import Module from 'node:module';
 
 const require = Module.createRequire(import.meta.url);
 const fixturesRoot = dirname(require.resolve('@ts-tools/fixtures/package.json'));
-const nodeMajorVersion = parseInt(process.versions.node.split('.')[0]!, 10);
 
 export function runCommand(command: string): { output: string; exitCode: number } {
   const [execName, ...args] = command.split(' ');
@@ -14,39 +13,37 @@ export function runCommand(command: string): { output: string; exitCode: number 
   return { output: output.join('\n'), exitCode: exitCode || 0 };
 }
 
-if (nodeMajorVersion >= 16) {
-  describe('using node --experimental-loader @ts-tools/esm <file>', function () {
-    this.timeout(5000);
+describe('using node --experimental-loader @ts-tools/esm <file>', function () {
+  this.timeout(5000);
 
-    describe('when tsconfig.json is found', () => {
-      it('allows using imports (with default interop)', () => {
-        const filePath = join(fixturesRoot, 'esm/imports.mts');
+  describe('when tsconfig.json is found', () => {
+    it('allows using imports (with default interop)', () => {
+      const filePath = join(fixturesRoot, 'esm/imports.mts');
 
-        const { output, exitCode } = runCommand(`node --experimental-loader @ts-tools/esm ${filePath}`);
+      const { output, exitCode } = runCommand(`node --experimental-loader @ts-tools/esm ${filePath}`);
 
-        expect(exitCode).to.equal(0);
-        expect(output).to.include(`Current platform is: ${platform()}`);
-        expect(output).to.include(`Path separator is: ${sep}`);
-      });
+      expect(exitCode).to.equal(0);
+      expect(output).to.include(`Current platform is: ${platform()}`);
+      expect(output).to.include(`Path separator is: ${sep}`);
+    });
 
-      it('maps stack traces using source maps when specifying --enable-source-maps', () => {
-        const filePath = join(fixturesRoot, 'esm/throwing.mts');
+    it('maps stack traces using source maps when specifying --enable-source-maps', () => {
+      const filePath = join(fixturesRoot, 'esm/throwing.mts');
 
-        const { output, exitCode } = runCommand(
-          `node --experimental-loader @ts-tools/esm --enable-source-maps ${filePath}`
-        );
+      const { output, exitCode } = runCommand(
+        `node --experimental-loader @ts-tools/esm --enable-source-maps ${filePath}`
+      );
 
-        expect(exitCode).to.not.equal(0);
-        expect(output).to.include(`runMe (${filePath}:10:11)`);
-      });
+      expect(exitCode).to.not.equal(0);
+      expect(output).to.include(`runMe (${filePath}:10:11)`);
+    });
 
-      it('does not throw on empty files', () => {
-        const filePath = join(fixturesRoot, 'esm/empty.mts');
+    it('does not throw on empty files', () => {
+      const filePath = join(fixturesRoot, 'esm/empty.mts');
 
-        const { exitCode, output } = runCommand(`node --experimental-loader @ts-tools/esm ${filePath}`);
+      const { exitCode, output } = runCommand(`node --experimental-loader @ts-tools/esm ${filePath}`);
 
-        expect(exitCode, output).to.equal(0);
-      });
+      expect(exitCode, output).to.equal(0);
     });
   });
-}
+});
