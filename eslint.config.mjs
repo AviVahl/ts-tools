3b3d9663cbb250d101700aa56af388cb051329b5
Avi Vahl <avi.vahl@wix.com>
2024-08-19 15:56:27 +0300

    infra: re-enable lint for js/mjs/cjs and add+enable no-only-tests plugin

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,38 @@
 import pluginJs from '@eslint/js';
-import tseslint from 'typescript-eslint';
 import configPrettier from 'eslint-config-prettier';
+import pluginNoOnlyTests from 'eslint-plugin-no-only-tests';
+import pluginTypescript from 'typescript-eslint';
+
+for (const config of pluginTypescript.configs.recommendedTypeChecked) {
+  config.files = ['**/*.{ts,tsx,mts,cts}']; // ensure config only targets TypeScript files
+}
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { ignores: ['**/dist/', '**/*.{js,mjs,cjs}', 'packages/fixtures/'] },
+  { ignores: ['**/dist/', 'packages/fixtures'] },
   pluginJs.configs.recommended,
-  ...tseslint.configs.recommendedTypeChecked,
-  { languageOptions: { parserOptions: { projectService: true } } },
-  { rules: { 'no-console': 'error', '@typescript-eslint/unbound-method': 'off' } },
+  { plugins: { 'no-only-tests': pluginNoOnlyTests } },
   {
-    files: ['**/*.test.{ts,mts,mts}'],
     rules: {
+      'no-console': 'error',
+      'no-only-tests/no-only-tests': 'error',
+      'no-undef': 'off',
+      'no-unused-vars': ['error', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+    },
+  },
+  ...pluginTypescript.configs.recommendedTypeChecked,
+  { languageOptions: { parserOptions: { projectService: true } } },
+  {
+    files: ['**/*.{ts,tsx,mts,cts}'],
+    rules: {
+      '@typescript-eslint/unbound-method': 'off',
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+    },
+  },
+  {
+    files: ['**/*.test.{ts,tsx,mts,cts}'],
+    rules: {
+      // native node test runner types for describe() and it() return a promise, so disable this rule in tests
       '@typescript-eslint/no-floating-promises': 'off',
     },
   },
